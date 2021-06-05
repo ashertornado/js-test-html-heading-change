04571515d994fafff230c4d1627cb98e7b405954
ashertornado <asher@ourjohnsonfamily.net>
2021-06-04 22:03:32 -0500

    files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Telling the console the app is working
+console.log("JavaScript application is running");
+
+// Heading text change
+document.getElementById("textChanger").onclick = function(){
+    document.getElementById("heading").innerHTML = "Heading was changed by JavaScript!";
+}
